fea88d1d68de1ac152645824782e5cb975141cda
ibCom <hello@ibcom.biz>
2024-02-08 22:53:14 +1100

    Update index.js

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ exports.handler = function (event, context, callback)
 
 					event._posts.genai = _.filter(event._posts.all, function (post)
 					{
-						return (_.includes(_.toLower(post.subject), '@genai:') && !_.includes(_.toLower(post.subject), '@genai:response:'))
+						return (_.includes(_.toLower(post.subject), '@genai:'))
 					});
 
 					if (event._posts.genai.length == 0)
